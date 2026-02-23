4188e2a7c17fb0207b81c7ce5815dbe6421cd0c0
Eray Aydın <eray.aydin@fingerprint.com>
2026-02-23 13:09:45 +0300

    chore: add new keyword to type error throw
    Add `new` keyword to the `deleteVisitorData` throwing Type Error.
    
    Related-Task: INTER-1488

--- src/serverApiClient.ts
+++ src/serverApiClient.ts
@@ -161,7 +161,7 @@ export class FingerprintJsServerApiClient implements FingerprintApi {
    */
   public async deleteVisitorData(visitorId: string): Promise<void> {
     if (!visitorId) {
-      throw TypeError('VisitorId is not set')
+      throw new TypeError('VisitorId is not set')
     }
 
     return this.callApi({
