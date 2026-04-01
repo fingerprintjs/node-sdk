83b006b7063d7bf24226d4239814891f6774940a
ilfa <darkilfa@gmail.com>
2026-04-01 17:24:21 +0000

    Deploying to gh-pages from @ fingerprintjs/node-sdk@dbb60e9a3f58196ef72a1ac3a3dffa31a08e05af 🚀

---
+++ media/unsealResult.mjs
@@ -0,0 +1,24 @@
+import { unsealEventsResponse, DecryptionAlgorithm } from '@fingerprint/node-sdk'
+import { config } from 'dotenv'
+config()
+
+const sealedData = process.env.BASE64_SEALED_RESULT
+const decryptionKey = process.env.BASE64_KEY
+
+if (!sealedData || !decryptionKey) {
+  console.error('Please set BASE64_KEY and BASE64_SEALED_RESULT environment variables')
+  process.exit(1)
+}
+
+try {
+  const unsealedData = await unsealEventsResponse(Buffer.from(sealedData, 'base64'), [
+    {
+      key: Buffer.from(decryptionKey, 'base64'),
+      algorithm: DecryptionAlgorithm.Aes256Gcm,
+    },
+  ])
+  console.log(JSON.stringify(unsealedData, null, 2))
+} catch (e) {
+  console.error(e)
+  process.exit(1)
+}
